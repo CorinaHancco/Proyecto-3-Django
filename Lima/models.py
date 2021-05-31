800b095d30482dac1e2015e3046a1903b4874032
Corina Hancco <chanccov@unsa.edu.pe>
2021-05-31 15:29:33 -0500

    Corregi una mala escritura

--- Lima/models.py
+++ Lima/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Estudinates(models.Model):
+class Estudiantes(models.Model):
     nombres = models.TextField() # es calmo es una caja de texto
     apellidos = models.TextField()
     edad = models.TextField()
