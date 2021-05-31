eea88c7864920daa6d471c25e6a0376bab846c19
Corina Hancco <chanccov@unsa.edu.pe>
2021-05-31 15:11:11 -0500

    Cree el modelo de tabla estudiantes con tres caracteristicas nombres, apellidos,edad

--- Lima/models.py
+++ Lima/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Estudinates(models.Model):
+    nombres = models.TextField() # es calmo es una caja de texto
+    apellidos = models.TextField()
+    edad = models.TextField()
